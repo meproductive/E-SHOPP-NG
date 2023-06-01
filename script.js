9b7c6bbd1f0bdf984d4d01ca42170dd9b4e33853
Berkay Yıldırım <114169639+meproductive@users.noreply.github.com>
2023-06-01 23:45:25 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,32 +1,55 @@
-
 const menu = document.querySelector(".menu");
+const cartcontent = document.querySelector(".cart-content");
+const searchdpcontent = document.querySelector(".search-dp-content");
 
+const dpbtn = document.querySelector(".dp-btn");
+const bagdpbtn = document.querySelector(".bag-dp-btn");
 const respicon = document.querySelector(".resp-icon");
-
 const respxicon = document.querySelector(".resp-x-icon");
 
 respicon.addEventListener("click", function() {
-    menu.classList.toggle("active");
-    document.addEventListener("click", function (e) {
-        if (!e.composedPath().includes(respicon) &&
-            !e.composedPath().includes(menu)
-            )
+  menu.classList.toggle("active");
+  document.addEventListener("click", function (e) {
+      if (!e.composedPath().includes(respicon) &&
+          !e.composedPath().includes(menu)
+          )
 respxicon.addEventListener("click", function() {
-    menu.classList.remove("active");
-        });
-    });
+  menu.classList.remove("active");
+      });
+  });
+});
+
+
+bagdpbtn.addEventListener("click", function() {
+  cartcontent.classList.toggle("active");
+  document.addEventListener("click", function (c) {
+    if (!c.composedPath().includes(bagdpbtn) &&
+        !c.composedPath().includes(cartcontent)
+      ) {
+          cartcontent.classList.remove("active");
+      }
+  });
+});
+
+dpbtn.addEventListener("click", function() {
+  searchdpcontent.classList.toggle("active");
+  document.addEventListener("click", function (b) {
+    if (!b.composedPath().includes(dpbtn) &&
+        !b.composedPath().includes(searchdpcontent)
+      ) {
+          searchdpcontent.classList.remove("active");
+      }
+  });
 });
 
 
 let slideIndex = 1;
 showSlides(slideIndex);
 
-// Next/previous controls
 function plusSlides(n) {
   showSlides(slideIndex += n);
 }
 
-// Thumbnail image controls
 function currentSlide(n) {
   showSlides(slideIndex = n);
 }
@@ -46,3 +69,22 @@ function showSlides(n) {
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
 }
+
+function imaFunction() {
+  document.getElementById("iamDropdown").classList.toggle("show");
+}
+
+windows.onclick = function(event) {
+  if (!event.target.matches('.drop-btn')) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
