3465d6a21a40eee46b1d3d4b9999154681612c1c
Berkay Yıldırım <114169639+meproductive@users.noreply.github.com>
2023-06-18 16:51:50 +0300

    script.js

--- script.js
+++ script.js
@@ -162,50 +162,41 @@ function myFunction(imgs) {
 }
 
 function openPage(pageName, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
+
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
   for (i = 0; i < tabcontent.length; i++) {
     tabcontent[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
   tablinks = document.getElementsByClassName("tablink");
   for (i = 0; i < tablinks.length; i++) {
     tablinks[i].style.backgroundColor = "";
   }
 
-  // Show the specific tab content
   document.getElementById(pageName).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementById("defaultOpen").click();
 
 
 function Tab(page, elmnt, color) {
-  // Hide all elements with class="tabcontent" by default */
   var i, paneltab, panellink;
   paneltab = document.getElementsByClassName("paneltab");
   for (i = 0; i < paneltab.length; i++) {
     paneltab[i].style.display = "none";
   }
 
-  // Remove the background color of all tablinks/buttons
   panellink = document.getElementsByClassName("panel-link");
   for (i = 0; i < panellink.length; i++) {
     panellink[i].style.backgroundColor = "";
   }
 
-  // Show the specific tab content
   document.getElementById(page).style.display = "block";
 
-  // Add the specific color to the button used to open the tab content
   elmnt.style.backgroundColor = color;
 }
 
-// Get the element with id="defaultOpen" and click on it
 document.getElementsById("active-button").click();
